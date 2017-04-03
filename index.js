a28ad58acec0ad6db3de09fedd601a2c12a6cb8c
raghavarao <rmandava@rivier.edu>
2017-04-02 23:55:20 -0500

    chat app

---
+++ index.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.emit('some event', { for: 'everyone' });
+
+io.on('connection', function(socket){
+  socket.broadcast.emit('raghava');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3333, function(){
+  console.log('listening on *:3333');
+});
